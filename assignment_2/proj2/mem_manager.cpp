e4ea2d7e7bd3b10d019c61800c3586fb538d6cff
ivan-analytics <penguinlgb@gmail.com>
2024-10-15 19:34:24 -0400

    mem_manager code done

--- assignment_2/proj2/mem_manager.cpp
+++ assignment_2/proj2/mem_manager.cpp
@@ -4,6 +4,35 @@
 using namespace std;
 
 
+pair<int, int> init_prompt() {
+	int num_input, len_input;
+	cout << "Memory configuration:" << endl;
+	cout << "Number of memory blocks (integer): ";
+	cin >> num_input;
+	cout << endl;
+	while (cin.fail()) {
+		// clearing the trails of invalid input
+        std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+		cout << "Number of memory blocks (integer): ";
+		cin >> num_input;
+		cout << endl;
+	}
+
+	cout << "Memory block size (integer): ";
+	cin >> len_input;
+	if (cin.fail()) {
+		// clearing the trails of invalid input
+        std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+		return init_prompt();
+	}
+
+	return make_pair(num_input, len_input);
+}
+
 void print_menu() {
 	cout << string(20, '=') << endl;
 	cout << "d: memory dump" << endl;
@@ -14,30 +43,54 @@ void print_menu() {
 	cout << "Your choice: ";	
 }
 
+
 int main() {
+	auto res = init_prompt();
+	int num = res.first;
+	int len = res.second;
 
-	// print_menu();
+	// create a memory object
+	MyMemory memory = MyMemory(num, len);
 
-	MyMemory m = MyMemory(8, 20);
-	m.dump();
+	string command_input_str;
+	char command_input;
+	int ind_, len_;
+	while (true) {
+		print_menu();
+		cin >> command_input_str;
+		command_input = command_input_str[0];
+		cout << "choice: " << command_input << endl;
 
-	m.req_mem(7);
-	m.req_mem(20);
-	m.req_mem(14);
-	m.req_mem(14);
-	m.req_mem(14);
-	m.req_mem(14);
-	m.req_mem(14);
-	m.dump();
-
-	// m.release_mem(0, 5);
-	// m.release_mem(1, 5);
-	// m.release_mem(1, 20);
-	// m.release_mem(1, 21);
-	// m.release_mem(5, 5);
-	// m.dump();
-	m.merge_mem();
-	m.dump();
-
-	return 0;
+		switch (command_input) {
+			case 'd':
+				// d: memory dump
+				memory.dump();
+				break;
+			case 'r':
+				// r: request memory
+				cout << "Requested memory size (integer): ";
+				cin >> len_;
+				memory.req_mem(len_);
+				break;
+			case 'l':
+				// l: release memory
+				cout << "Release memory index (integer): ";
+				cin >> ind_;
+				cout << "Release memory length (integer): ";
+				cin >> len_;
+				memory.release_mem(ind_, len_);
+				break;
+			case 'm':
+				// m: merge memory
+				memory.merge_mem();
+				break;
+			case 'x':
+				// x: quit program
+				return 1;
+			default:
+				cout << endl << "*****Error: Invalid entry.  Try again." << endl;
+		}
+	}
+	
+	return 1;
 }
