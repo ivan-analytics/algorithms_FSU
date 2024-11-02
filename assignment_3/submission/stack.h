8f705c7ac7fb3f8aec10ef7177f4ccfa717d9848
ivan-analytics <penguinlgb@gmail.com>
2024-11-02 14:07:23 -0400

    code submission files ready and finalized

---
+++ assignment_3/submission/stack.h
@@ -0,0 +1,58 @@
+#ifndef DL_STACK_H
+#define DL_STACK_H
+#include <iostream>
+#include <vector>
+
+namespace cop4530 {
+	template <typename T>
+		class Stack {
+			public:
+            Stack(); // zero-argument constructor.
+            Stack (const Stack<T>& other); // copy constructor.   
+            Stack(Stack<T>&& other); // move constructor.
+            
+            ~Stack(); // destructor
+
+            Stack<T>& operator=(const Stack <T>& other); // copy assignment operator=
+            Stack<T>& operator=(const Stack <T>&& other); // move assignment operator=
+            
+            bool empty() const; // returns true if the Stack contains no elements, and false otherwise  
+            void clear(); // delete all elements from the stack
+
+            void push(const T& x); // adds x to the Stack copy version
+            void push(const T&& x); // adds x to the Stack copy version
+
+            void pop(); // removes and discards the most recently added element of the Stack
+            T& top(); // mutator that returns a reference to the most recently added element of the Stack
+            const T& top() const; // accessor that returns the most recently added element of the Stack  
+            
+            int size() const; // returns the number of elements stored in the Stack
+
+            // print elements of Stack to ostream os. ofc is the separator between elements in the stack when they are printed out 
+            // Note that print() prints elements in the opposite order of the Stack (that is, the oldest element should be printed first)
+            void print(std::ostream& os, char ofc = ' ') const;
+            
+            private:
+            std::vector<T> elements;
+        };
+
+    template <typename T>
+    std::ostream& operator<<(std::ostream& os, const Stack<T>& a); // invokes the print() method to print the Stack<T> a in the specified ostream    
+
+    template <typename T>
+    bool operator==(const Stack<T>& s1, const Stack<T>& s2); // returns true if the two compared Stacks have the same elements, in the same order  
+    
+    template <typename T>
+    bool operator!=(const Stack<T>& s1, const Stack<T>& s2); // opposite of operator==()
+    
+    // returns true if every element in Stack s1 is smaller than or equal to the corresponding element of Statck s2, 
+    // i.e., if repeatedly invoking top() and pop() on both s1 and s2,  
+    // we will generate a sequence of elements s1_i from s1 and s2_i from s2, and for every i,  s1_i <= s2_i, until s1 is empty.
+    template <typename T>
+    bool operator<=(const Stack<T>& s1, const Stack<T>& s2);
+
+    // including the implementation file
+    #include "stack.hpp"
+}
+
+#endif
---
+++ assignment_3/submission/stack.hpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <stdexcept>
+
+
+template <typename T>
+cop4530::Stack<T>::Stack() {
+    this->elements = std::vector<T>{};
+} // zero-argument constructor.
+
+template <typename T>
+cop4530::Stack<T>::Stack(const Stack<T>& other) {
+    this->elements = other.elements;
+} // copy constructor.   
+
+template <typename T>
+cop4530::Stack<T>::Stack(Stack<T> &&other) {
+    this->elements = std::move(other.elements);
+} // move constructor.
+
+template <typename T>
+cop4530::Stack<T>::~Stack() {
+    // vector container will delete elements on its own
+} // destructor
+
+template <typename T>
+cop4530::Stack<T>& cop4530::Stack<T>::operator=(const Stack <T>& other) {
+    if (this != &other)
+        this->elements = other.elements;
+    return *this;
+} // copy assignment operator=
+
+template <typename T>
+cop4530::Stack<T>& cop4530::Stack<T>::operator=(const Stack <T>&& other) {
+    if (this != &other)
+        this->elements = std::move(other.elements);
+    return *this;
+} // move assignment operator=
+
+template <typename T>
+bool cop4530::Stack<T>::empty() const {
+    return elements.empty();
+} // returns true if the Stack contains no elements, and false otherwise  
+
+template <typename T>
+void cop4530::Stack<T>::clear() {
+    this->elements.clear();
+} // delete all elements from the stack
+
+template <typename T>
+void cop4530::Stack<T>::push(const T& x) {
+    this->elements.push_back(x);
+} // adds x to the Stack copy version
+
+template <typename T>
+void cop4530::Stack<T>::push(const T&& x) {
+    this->elements.push_back(std::move(x));
+} // adds x to the Stack copy version
+
+template <typename T>
+void cop4530::Stack<T>::pop() {
+    if (this->elements.empty()) throw std::runtime_error("pop() is called but stack is already empty");
+    this->elements.pop_back();
+} // removes and discards the most recently added element of the Stack
+
+template <typename T>
+T& cop4530::Stack<T>::top() {
+    if (this->elements.empty()) throw std::runtime_error("top() is called but stack is empty");
+    return this->elements.back();
+} // mutator that returns a reference to the most recently added element of the Stack
+
+
+template <typename T>
+const T& cop4530::Stack<T>::top() const {
+    if (this->elements.empty()) throw std::runtime_error("top() is called but stack is empty");
+    return this->elements.back();
+} // accessor that returns the most recently added element of the Stack  
+
+template <typename T>
+int cop4530::Stack<T>::size() const {
+    return this->elements.size();
+} // returns the number of elements stored in the Stack
+
+// print elements of Stack to ostream os. ofc is the separator between elements in the stack when they are printed out 
+// Note that print() prints elements in the opposite order of the Stack (that is, the oldest element should be printed first)
+template <typename T>
+void cop4530::Stack<T>::print(std::ostream& os, char ofc) const {
+    for (auto& el : this->elements)
+        os << el << ofc;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const cop4530::Stack<T>& a) {
+    a.print(os);
+    return os;
+} // invokes the print() method to print the Stack<T> a in the specified ostream    
+
+template <typename T>
+bool operator==(const cop4530::Stack<T>& s1, const cop4530::Stack<T>& s2) {
+    // checking the first pair of elements before copying two stacks for higher efficiency
+    // hence starting the loop from i=1 instead of 0
+    if (s1.empty() && s2.empty()) return true;
+    if ((s1.size() != s2.size()) || (s1.top() != s2.top())) return false;
+    
+    cop4530::Stack<T> s1_ = s1;
+    cop4530::Stack<T> s2_ = s2;
+    s1_.pop();
+    s2_.pop();
+    // picking s1.size() since the sizes should be equal at this point 
+    for (int i = 1; i < s1.size(); i++) {
+        if (s1_.top() != s2_.top()) return false;
+        s1_.pop();
+        s2_.pop();
+    }
+
+    // stacks are equal
+    return true;
+} // returns true if the two compared Stacks have the same elements, in the same order  
+
+template <typename T>
+bool operator!=(const cop4530::Stack<T>& s1, const cop4530::Stack<T>& s2) {
+    return !(s1 == s2);
+} // opposite of operator==()
+
+// returns true if every element in Stack s1 is smaller than or equal to the corresponding element of Statck s2, 
+// i.e., if repeatedly invoking top() and pop() on both s1 and s2,  
+// we will generate a sequence of elements s1_i from s1 and s2_i from s2, and for every i,  s1_i <= s2_i, until s1 is empty.
+template <typename T>
+bool operator<=(const cop4530::Stack<T>& s1, const cop4530::Stack <T>& s2) {
+    // checking the first pair of elements before copying two stacks for higher efficiency
+    // hence starting the loop from i=1 instead of 0
+    if (s1.empty() && s2.empty()) return true;
+    if (s1.empty() || s2.empty()) return false; // if one is empty, but not both - stop right there and return false as they clearly are not equal
+    if (s1.top() > s2.top()) return false;
+    
+    cop4530::Stack<T> s1_ = s1;
+    cop4530::Stack<T> s2_ = s2;
+    s1_.pop();
+    s2_.pop();
+    // picking s1.size() since we check only until s1 is empty as instructed in assignment requirements
+    for (int i = 1; i < s1.size(); i++) {
+        if (s1_.top() > s2_.top()) return false;
+        s1_.pop();
+        s2_.pop();
+    }
+
+    return true;
+}
