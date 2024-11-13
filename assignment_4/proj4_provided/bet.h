bb08cb0577a5d9a2e839d45f988aff5386550b8b
ivan-analytics <penguinlgb@gmail.com>
2024-11-12 19:56:43 -0500

    init

---
+++ assignment_4/proj4_provided/bet.h
@@ -0,0 +1,18 @@
+#ifndef DL_BET_H
+#define DL_BET_H
+
+namespace cop4530 {
+	template <typename T>
+		class BET {
+			public:
+            
+            
+            private:
+
+        };
+
+    // including the implementation file
+    #include "bet.hpp"
+}
+
+#endif
